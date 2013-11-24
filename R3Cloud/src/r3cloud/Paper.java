786a7beb83b373a424e2ee75d31b192fd9575927
Nada-Ossama-Kamal <nada.ossama.kamal@gmail.com>
2013-11-24 19:50:29 +0100

    trial

--- R3Cloud/src/r3cloud/Paper.java
+++ R3Cloud/src/r3cloud/Paper.java
@@ -57,12 +57,11 @@ public class Paper {
 		String date = df.format(now);
 		this.date = date;
 		this.text = text;
-		if (text){
+		if (text)
 			this.content = content;
-		}
-		else{
+		else
 			this.url = url;
-		}
+		
 		
 		this.abstractPaper = abstractPaper;
 		this.owner = owner;
